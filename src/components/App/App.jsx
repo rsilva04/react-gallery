a0bd8a433936c12593ef7d9600322b8dfcc2b9b9
Rafael Silva <rafsilva004@gmail.com>
2023-09-05 16:41:51 -0500

    GET request made

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios'
+
 
 function App() {
+
+  axios.get('/gallery')
+  .then(response => {
+    // Handle the successful response here
+    console.log('Data received:', response.data);
+  })
+  .catch(error => {
+    // Handle any errors that occurred during the request
+    console.error('Error:', error);
+  });
+
+
     return (
       <div className="App">
         <header className="App-header">
