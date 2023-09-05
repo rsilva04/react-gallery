63a79bca8ae6a2d73f928f2e646f49ae779a0574
Rafael Silva <rafsilva004@gmail.com>
2023-09-04 21:36:20 -0500

    added information to each picture in gallery data

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,11 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/concert.jpeg', description: 'Photo of me taken at a Steve Aoki Concert in Wayzata.', likes: 0 },
+    { id: 3, path: 'images/mn_vikings.jpeg', description: 'Photo at the Minnesota Vikings Game.', likes: 0 },
+    { id: 4, path: 'images/parlour.jpeg', description: 'Photo of me taken at my favorite restaurant, Parlour.', likes: 0 },
+    { id: 5, path: 'images/state_fair.jpeg', description: 'Photo of me taken at the Minnesota State Fair.', likes: 0 },
+    { id: 6, path: 'images/sunset.jpeg', description: 'Sunset picture taken at Nokomis Lake.', likes: 0 }
+
 ];
 
 module.exports = galleryItems;
